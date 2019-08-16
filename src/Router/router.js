1fa87bdc74748675c8f12679a763dee36a39f22b
lagggggggggg <810095295@qq.com>
2019-08-16 16:48:20 +0800

    project init

---
+++ src/Router/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../pages/Home/Home'
+
+
+Vue.use(VueRouter)
+const routes=[
+    {path:'/', component:Home },
+]
+
+export default new VueRouter({
+    mode:'history',
+    routes
+})
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './Router/router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+      fastClick.attach(document.body);
+  }, {passive:false});
+}
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
